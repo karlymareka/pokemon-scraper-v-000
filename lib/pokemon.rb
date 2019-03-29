a52a5eaeb35a48757aed208bde00a9175ae668c3
Karly Mareka <karly.mareka@gmail.com>
2019-03-29 16:22:42 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -19,7 +19,7 @@ class Pokemon
     new_pokemon = db.execute("SELECT id, name, type, hp FROM pokemon")
     new_pokemon.each do |creature|
       if creature[0] == id
-        return self.new(id: creature[0], name: creature[1], type: creature[2], hp: creature[3])
+        return self.new(id: creature[0], name: creature[1], type: creature[2], db: creature[3])
       end
     end 
   end
